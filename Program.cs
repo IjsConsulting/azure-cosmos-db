ca3e023e1d3bfd01606566a824be424e5fd7d9cc
ian simpson <iansimpson55@hotmail.com>
2020-06-02 10:17:43 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cosmosdb_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
